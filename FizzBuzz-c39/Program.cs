5c05e5a97e47e10f2238d60abdba1843c61889f8
Greg Doud <gpdoud@gmail.com>
2023-02-14 12:07:46 -0500

    Add project files.

---
+++ FizzBuzz-c39/Program.cs
@@ -0,0 +1,28 @@
+﻿
+for(int idx = 1; idx <= 30; idx++) {
+
+    bool isDivBy3 = (idx % 3 == 0) ? true : false;
+
+    bool isDivBy5 = (idx % 5 == 0) ? true : false;
+
+    bool isDivBy3n5 = isDivBy3 && isDivBy5;
+
+    //==========================================//
+
+    if (isDivBy3n5) {
+        Console.Write(" FIZZBUZZ ");
+        continue;
+    }
+
+    if(isDivBy3) {
+        Console.Write(" FIZZ ");
+        continue;
+    }
+    if(isDivBy5) {
+        Console.Write(" BUZZ ");
+        continue;
+    }
+
+    Console.Write($" {idx} ");
+
+}
